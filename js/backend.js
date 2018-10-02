f0a04f6e1e3510d88c882fdba2e047c66b8d8301
Mikhail <mihail_kotov_86@mail.ru>
2018-10-02 23:18:43 +0300

    Функция обработки xhr

---
+++ js/backend.js
@@ -0,0 +1,37 @@
+'use strict';
+
+(function () {
+  var URL = 'https://js.dump.academy/keksobooking/data';
+
+  var getXhrData = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      switch (xhr.status) {
+        case 200:
+          onLoad(xhr.response);
+          break;
+        case 400:
+          onError('Неверный запрос');
+          break;
+        case 404:
+          onError('Ничего не найдено');
+          break;
+        default:
+          onError('Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Что-то пошло не так! Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = window.constants.TIMEOUT;
+
+    return xhr;
+  };
+
+})();
